5510fe77437e399bb6ea4d7c10084f7f5a6d1226
np01cp4s220062@islingtoncollege.edu.np <hamalaavash03@gmail.com>
2024-04-17 18:40:10 +0545

    Commit for all

---
+++ HamroPharma.API/Models/DTO/PurchaseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HamroPharma.API.Models.DTOs
+{
+    public class PurchaseDto
+    {
+        public int? Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Guid VendorId { get; set; }
+        public Guid ProductId { get; set; }
+    }
+}
